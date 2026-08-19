f822a42b83d1e13feef7904eaaba4213f0b68ce4
KarwanKamboj <karwankamboj@gmail.com>
2026-08-19 19:45:21 +0530

    include and extends in jinja

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)  # app is an object(instance) of application created
                         # flask uses it to locate templates,static and other files
 @app.route("/")   # decorater, to execute particular function when user visits the url
 def home():       # / represents homepage of website
-    return render_template("index.html",name ="Karwan") # sends response to the web browser
+    return render_template("index.html") # sends response to the web browser
                         # html pages using templates can also be returned such as return render_template("index.html")
 if __name__ == "__main__": # to check that file is executed directly,not imported to other file
     app.run(debug=True)  # enables debug mode ,so that whenever there is changes the server automatically loads
